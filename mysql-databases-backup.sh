368a21720a22924426c9dacef2cc4476abc3ce60
metodiew <stanko@metodiew.com>
2016-10-12 20:13:10 +0300

    Initial commit of  mysql-databases-backup.sh, a script for automated databases backup

---
+++ mysql-databases-backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# The script is using for creating separate files for each database files, using mysqldump
+# This version is inspired from http://stackoverflow.com/questions/9497869/export-and-import-all-mysql-databases-at-one-time and slighly modified to meet my need
+
+USER="root"
+PASSWORD=" " # Enter your MySQL password here
+
+databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+
+for db in $databases; do
+    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
+        echo "Dumping database: $db"
+        mysqldump -u $USER  --databases $db > `date +%Y%m%d`.$db.sql
+    fi
+done
