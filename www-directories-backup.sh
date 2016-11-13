986262037b0a175ed7ee129748854520280dce66
metodiew <stanko@metodiew.com>
2016-11-13 03:39:43 +0200

    Increase the pause for www-directories-backup.sh script

--- www-directories-backup.sh
+++ www-directories-backup.sh
@@ -16,7 +16,7 @@ for dir in `find . -maxdepth 1 -type d  | grep -v "^\.$" `;
 	do
 		# Create the archive
 		echo 'Starting with' ${dir//.\/}'-'$NOW'.zip archive ...';
-		sleep 2;				
+		sleep 5;			
 		zip -r ${dir}-$NOW.zip ${dir};
 		
 		# Move the created archive to a specific directory
