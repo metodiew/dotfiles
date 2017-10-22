d742043a3d70647b806b1adc1f03dd64ce90d14f
metodiew <stanko@metodiew.com>
2017-10-22 21:11:57 +0300

    Adjust www-apache-directories-backup.sh sript

--- www-apache-directories-backup.sh
+++ www-apache-directories-backup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+#
+# Script Name: www-apache-directories-backup.sh
+#
+# Author: Stanko Metodiev
+# Author Email: stanko@metodiew.com
+# Author URL: https://metodiew.com
+# Date : 2017.10.22
+# Description: Prepare an archive file for each html Apache directory.
 
-# @Author Stanko Metodiev <stanko@metodiew.com>
-
-# Creating a separate zip archive for each directory from Localhost server, in my case /var/www/html/ :)
-# I'm storing the backups to my HDD, from where I'm copying them to an external drive, but you can skip
-# the `mv` part
-
-# Do we need to explain this one?
+# Get the today's date
 NOW="`date +%Y%m%d`";
 
+mkdir '/mnt/5DB56B841BB28CF1/Backup Files/WWW Backup/Apache/www-directory-backup/'$NOW;
+cd /var/www/html;
 for dir in `find . -maxdepth 1 -type d  | grep -v "^\.$" `;
 	do
 		# Create the archive
@@ -19,8 +23,9 @@ for dir in `find . -maxdepth 1 -type d  | grep -v "^\.$" `;
 		# Move the created archive to a specific directory
 		# Most likely in your case this will be different
 		echo 'Moving' ${dir//.\/}'-'$NOW'.zip archive ...';
-		mv ${dir}-$NOW.zip '/mnt/5DB56B841BB28CF1/Backup Files/WWW Backup/Apache/www-directory-backup-latest/';
+		mv ${dir}-$NOW.zip '/mnt/5DB56B841BB28CF1/Backup Files/WWW Backup/Apache/www-directory-backup/'$NOW;
 
 		echo 'Ready with' ${dir//.\/}'-'$NOW'.zip archive ...';
 		sleep 5;
 done
+cd '/mnt/5DB56B841BB28CF1/Backup Files/';
