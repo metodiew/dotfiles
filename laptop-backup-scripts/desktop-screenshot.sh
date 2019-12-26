225917d89d530e1fc59a621b253d83212dc10461
metodiew <stanko@metodiew.com>
2019-12-26 20:22:28 +0200

    desktop-screenshot.sh: update the script, use the variables file now and add the echo files in the proper file

--- laptop-backup-scripts/desktop-screenshot.sh
+++ laptop-backup-scripts/desktop-screenshot.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-#
-# Script Name: desktop-screenshot.sh
+
 #
 # Author: Stanko Metodiev
 # Author Email: stanko@metodiew.com
 # Author URL: https://metodiew.com
-# Date : 2018.02.10
+# Last Updated: 2019.12.26
 # Description: This is a script for taking a screenshot of my Desktop and move it to the backup files
 
-# Set the Backup directory
-BACKUPFOLDERROOT='/media/metodiew/metodiew HDD/Backup Files';
+# Get the variables from the separate file
+. ./metodiew-laptop-backup-variables.sh;
 
-# Get the today's date
-NOW="`date +%Y%m%d`";
+echo "Taking a Desktop Screenshot"
+sleep 2;
 
 # Take the screenshot
 import -window root desktop-screenshot-$NOW.png
 
 # Move the screenshot to Desktop Screenshots directory
 mv desktop-screenshot-$NOW.png  "$BACKUPFOLDERROOT/Desktop Screenshots/";
+
+echo 'Desktop Screenshot is ready.';
+sleep 2;
