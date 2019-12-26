58ce2fafd82796eb3c9bbc9db608e5720dc2a41c
metodiew <stanko@metodiew.com>
2019-12-26 20:37:49 +0200

    www-apache-directories-backup.sh: update the script and use the variables

--- laptop-backup-scripts/www-apache-directories-backup.sh
+++ laptop-backup-scripts/www-apache-directories-backup.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
-#
-# Script Name: www-apache-directories-backup.sh
+
 #
 # Author: Stanko Metodiev
 # Author Email: stanko@metodiew.com
 # Author URL: https://metodiew.com
-# Date : 2017.10.22
+# Last Updated: 2019.12.26
 # Description: Prepare an archive file for each html Apache directory.
 
-# Set the Backup directory
-BACKUPFOLDERROOT='/media/metodiew/metodiew HDD/Backup Files';
-
-# Get the today's date
-NOW="`date +%Y%m%d`";
+# Get the variables from the separate file
+. ./metodiew-laptop-backup-variables.sh;
 
 mkdir "$BACKUPFOLDERROOT/WWW Backup/Apache/www-directory-backup/"$NOW;
-cd /var/www/html;
+cd $APACHEFOLDER;
 for dir in `find . -maxdepth 1 -type d  | grep -v "^\.$" `;
 	do
 		# Create the archive
 		echo 'Starting with' ${dir//.\/}'-'$NOW'.zip archive ...';
-		sleep 5;			
+		sleep 2;
 		zip -r ${dir}-$NOW.zip ${dir};
-		
+
 		# Move the created archive to a specific directory
-		# Most likely in your case this will be different
 		echo 'Moving' ${dir//.\/}'-'$NOW'.zip archive ...';
 		mv ${dir}-$NOW.zip "$BACKUPFOLDERROOT/WWW Backup/Apache/www-directory-backup/"$NOW;
 
 		echo 'Ready with' ${dir//.\/}'-'$NOW'.zip archive ...';
-		sleep 5;
+		sleep 2;
 done
+
+# It's important to return to the backup folder, so we can continue with the rest of the backup
 cd "$BACKUPFOLDERROOT/";
