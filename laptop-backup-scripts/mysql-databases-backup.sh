c45e6e6044d150a2342f98e15cee4322643bbbd1
metodiew <stanko@metodiew.com>
2019-12-26 20:54:18 +0200

    mysql-databases-backup.sh: move the apache stop/start in the script file

--- laptop-backup-scripts/mysql-databases-backup.sh
+++ laptop-backup-scripts/mysql-databases-backup.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
-#
-# Script Name: mysql-databases-backup.sh
+
 #
 # Author: Stanko Metodiev
 # Author Email: stanko@metodiew.com
 # Author URL: https://metodiew.com
-# Lst Updated : 2019.12.26
+# Last Updated: 2019.12.26
 # Description: This is a script for backup of all Apache MySQL database from my localhost machine.
 
-# Set the Backup directory
-BACKUPFOLDERROOT='/media/metodiew/metodiew HDD/Backup Files';
-
-# Get the today's date
-NOW="`date +%Y%m%d`";
-
-# MySQL detailes here
-USER="root"
-PASSWORD="root" # Enter your MySQL password here
+# Get the variables from the separate file
+. ./metodiew-laptop-backup-variables.sh;
 
 ###########################################
 #### Let's start with the MySQL backup ####
 ###########################################
+
+# We need to start the Apache server, just in case.
+sudo service apache2 start;
 
 # Create a new folder
 mkdir $NOW;
@@ -30,7 +25,7 @@ databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
         echo "Dumping database: $db"
-	# Backup each database to the new created folder
+	    # Backup each database to the new created folder
         mysqldump -u $USER -p$PASSWORD $db > $NOW/$NOW.$db.sql
     fi
 done
@@ -39,3 +34,6 @@ done
 zip -r $NOW.zip $NOW;
 mv $NOW.zip "$BACKUPFOLDERROOT/WWW Backup/Apache/SQLs";
 rm -r $NOW;
+
+# We need to stop Apache server after the script. Just in case.
+sudo service apache2 stop;
